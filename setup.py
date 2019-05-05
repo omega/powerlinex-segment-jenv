efe31dea8cabaa69d4e52509bd31186cc6e4e35b
Andreas Marienborg <omega@palle.net>
2019-05-05 17:30:25 +0200

    Fix naming error in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='powerlinex-segment-plenv',
+        name='powerlinex-segment-jenv',
         packages=find_packages(),
         namespace_packages=['powerlinex']
         )
